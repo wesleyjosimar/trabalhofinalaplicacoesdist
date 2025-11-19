f3fbb3d27de6add43e7ade44b6c4ff5c5d6339dd
Wesley Josimar de Lima <Wesley.Lima@ad.brdrive.net>
2025-11-19 13:58:29 -0300

    Adiciona middlewares faltantes: RedirectIfAuthenticated e ValidateSignature

---
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectIfAuthenticated
+{
+    public function handle(Request $request, Closure $next, string ...$guards): Response
+    {
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (session()->has('usuario_id')) {
+                return redirect('/atletas');
+            }
+        }
+
+        return $next($request);
+    }
+}
+
